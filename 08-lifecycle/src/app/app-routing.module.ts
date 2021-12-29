ea30447d4bac3bc06fee40bfe243d591bc2f2c48
korrakeala <renata.mansilla@gmail.com>
2021-12-29 14:13:35 -0300

    07-selectores completo

--- 08-lifecycle/src/app/app-routing.module.ts
+++ 08-lifecycle/src/app/app-routing.module.ts
@@ -1,16 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
-  {
-    path: 'selector',
-    loadChildren: () => import('./paises/paises.module').then( m => m.PaisesModule )
-  },
-  {
-    path: '**',
-    redirectTo: 'selector'
-  }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
